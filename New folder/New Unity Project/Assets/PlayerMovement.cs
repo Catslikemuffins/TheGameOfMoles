6bebfe57c9aa418daae3c52894674cdaac45f724
Catslikemuffins <62771372+Catslikemuffins@users.noreply.github.com>
2020-04-01 10:54:22 -0700

    player movement

--- New folder/New Unity Project/Assets/PlayerMovement.cs
+++ New folder/New Unity Project/Assets/PlayerMovement.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
+    public float moveSpeed = 5f;
+
+    public Rigidbody2D rb;
+
+    Vector2 movement;
     // Update is called once per frame
     void Update()
     {
-        
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+    }
+
+
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
     }
 }
