e463189d9c84dbf0fdccb8473cfc03f34102e767
Catslikemuffins <62771372+Catslikemuffins@users.noreply.github.com>
2020-04-04 12:27:55 -0700

    Added move anim

--- New folder/New Unity Project/Assets/PlayerMovement.cs
+++ New folder/New Unity Project/Assets/PlayerMovement.cs
@@ -8,13 +8,17 @@ public class PlayerMovement : MonoBehaviour
     public float moveSpeed = 5f;
 
     public Rigidbody2D rb;
-
+    public Animator animator;
     Vector2 movement;
     // Update is called once per frame
     void Update()
     {
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
+
+        animator.SetFloat("Horizontal", movement.x);
+        animator.SetFloat("Vertical", movement.y);
+        animator.SetFloat("Speed", movement.sqrMagnitude);
     }
 
 
