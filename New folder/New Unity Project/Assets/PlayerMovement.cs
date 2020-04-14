61812eebc883a3c4b71a6789ae27c16d00263e90
Catslikemuffins <62771372+Catslikemuffins@users.noreply.github.com>
2020-04-14 10:42:28 -0700

    stone texture

--- New folder/New Unity Project/Assets/PlayerMovement.cs
+++ New folder/New Unity Project/Assets/PlayerMovement.cs
@@ -13,17 +13,20 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // Movement Controller
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
 
         animator.SetFloat("Horizontal", movement.x);
         animator.SetFloat("Vertical", movement.y);
         animator.SetFloat("Speed", movement.sqrMagnitude);
+
     }
 
 
     void FixedUpdate()
     {
         rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+        
     }
 }
